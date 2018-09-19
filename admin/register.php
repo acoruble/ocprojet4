d6cb347dcbd75864f4df567bef6b94de7361643a
Anne-Lise Coruble <38162196+acoruble@users.noreply.github.com>
2018-09-19 14:36:22 +0200

    Frontend et backend terminé en php procédural
    Bug pour modifier un billet à vérifier + passer en poo/mvc à faire

---
+++ admin/register.php
@@ -0,0 +1,15 @@
+<?php
+    require_once('../include/config.php');
+
+    $password = password_hash("motdepasse", PASSWORD_DEFAULT);
+    $username = "surnom";
+    $email = "test@test.com";
+
+    $sql = "INSERT INTO adminuser(username, password, email) VALUES (:username, :password, :email)";
+    $stmt = $dbh->prepare($sql);
+    $stmt->bindValue(':username', $username);
+    $stmt->bindValue(':password', $password);
+    $stmt->bindValue(':email', $email);
+    $stmt->execute();
+    echo $username . " admin user had been created";
+?>
